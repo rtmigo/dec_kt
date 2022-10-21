6c44b45ba1047df6ef9496b066c70a09d91d7f0c
Artem IG <ortemeo@gmail.com>
2022-10-22 02:29:45 +0300

    revive

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,52 @@ plugins {
     jacoco
     java
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20"
+    id("maven-publish")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withJavadocJar()
+    withSourcesJar()
 }
 
 group = "io.github.rtmigo"
-version = "0.0.0+1"
+version = "0.1.0-SNAPSHOT"
 
-tasks.register("pkgver") {
-    doLast {
-        println(project.version.toString())
+publishing {
+    publications {
+        create<MavenPublication>("dec") {
+            from(components["java"])
+            pom {
+                val github = "https://github.com/rtmigo/dec_kt"
+
+                name.set("repr")
+                description.set("Kotlin wrapper for Java BigDecimal")
+                url.set(github)
+
+                developers {
+                    developer {
+                        name.set("Artsiom iG")
+                        email.set("ortemeo@gmail.com")
+                        organization {
+                            this.name.set("Revercode")
+                            this.url.set("https://revercode.com")
+                        }
+                    }
+                }
+                scm {
+                    url.set(github)
+                    connection.set(github.replace("https:", "scm:git:"))
+                }
+                licenses {
+                    license {
+                        name.set("ISC License")
+                        url.set("$github/blob/HEAD/LICENSE")
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -33,7 +66,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("io.kotest:kotest-assertions-core:5.5.1")
-
 }
 
 kotlin {
@@ -91,4 +123,3 @@ tasks.register<Jar>("uberJar") {
                  .map { zipTree(it) }
          })
 }
-
